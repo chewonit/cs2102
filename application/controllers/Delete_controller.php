214106c30ee67192b32f0e5847cf88aec5e2279f
choohyc <charmainechy@hotmail.com>
2015-09-23 21:27:15 +0800

    Updated with delete function

---
+++ application/controllers/Delete_controller.php
@@ -0,0 +1,39 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Delete_controller extends CI_Controller {
+
+	private $site_title = "CS2102 Group 9 Delete";
+
+	public function view()
+	{
+		$page = 'delete_page';
+
+		if (!file_exists(APPPATH . '/views/pages/' . $page . '.php'))
+		{
+			// Whoops, page not found!
+			show_404();
+		}
+
+		$data['site_title'] = $this -> site_title;
+		$data['page_title'] = "Delete Controller Page";
+
+		/* Demo get from database */
+		$data['demo_list'] = $this -> demo_model -> get();
+
+		$this -> load -> view('templates/header', $data);
+		$this -> load -> view('pages/' . $page, $data);
+		$this -> load -> view('templates/footer', $data);
+	}
+	
+	public function delete()
+	{
+		$id = $this->input->post('inputJobId');
+		$this -> demo_model -> delete($id);
+		$result = $this -> demo_model -> get();
+
+		$this -> demo_model -> delete($data);
+		redirect('/demo/delete');
+	}
+	
+}
---
+++ application/views/pages/delete_page.php
@@ -0,0 +1,107 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+	<div class="row">
+		<div class="col-md-12">
+			<div class="page-header">
+				<h1><?php echo $page_title?></h1>
+			</div>
+		</div>
+	</div>
+
+	<div class="row">
+	
+		<div class="col-md-9">
+		    
+			<table id="jobTable" class="table table-striped">
+				<thead>
+					<th>Job ID</th>
+					<th>Company</th>
+					<th>Title</th>
+					<th>Description</th>
+					<th>Location</th>
+				</thead>
+				<?php $count = 1 ?>
+				<?php foreach($demo_list->result() as $row): ?>
+				<tr id="jobTable-<?php echo $row->Id; ?>">
+					<td><?php echo $row->Id; ?></td>
+					<td><?php echo $row->Name; ?></td>
+					<td><?php echo $row->Title; ?></td>
+					<td><?php echo $row->Description; ?></td>
+					<td><?php echo $row->Location; ?></td>
+					<td>
+						<button onclick="myFunction(<?php echo "'jobTable-".$row->Id."'";?>)" 
+							class="btn btn-default">
+								<span class="glyphicon glyphicon-remove" aria-hidden="true"></span>
+						</button>
+					</td>
+				</tr>
+				<?php $count += 1 ?>
+				<?php endforeach; ?>
+			</table>
+			
+			<script type="text/javascript">
+				var jq = $.noConflict();
+				
+				var formInputIds = ["#inputJobId", "#inputCompanyName", "#inputTitle", "#inputDescription", "#inputLocation"];
+				
+				function myFunction(id) {
+					jq('html,body').animate({
+						scrollTop: jq("#updateForm").offset().top
+					});
+					jq("#jobTable").find("#"+id).find('td').each( function(index) {
+						if (index < formInputIds.length) {
+							jq( formInputIds[index] ).val(jq( this ).html());
+						}
+					});
+				}
+			</script>
+			
+			
+			<h2>Delete Job Entry</h2>
+			
+			<?php echo form_open('Delete_controller/delete/');?>
+			<div id="updateForm" class="fluid-container">
+			<div class="row">
+				<div class="form-group col-md-6">
+					<label for="inputJobId">Job ID</label>
+					<input type="text" class="form-control" name="inputJobId" id="inputJobId" placeholder="Job ID" readonly>
+				</div>
+				<div class="form-group col-md-6">
+					<label for="inputCompanyName">Company Name</label>
+					<input type="text" class="form-control" name="inputCompanyName" id="inputCompanyName" placeholder="Company Name">
+				</div>
+			</div>
+			<div class="row">
+				<div class="form-group col-md-6">
+					<label for="inputTitle">Job Title</label>
+					<input type="text" class="form-control" name="inputTitle" id="inputTitle" placeholder="Job Title">
+				</div>
+				<div class="form-group col-md-6">
+					<label for="inputLocation">Location</label>
+					<input type="text" class="form-control" name="inputLocation" id="inputLocation" placeholder="Location">
+				</div>
+			</div>
+			<div class="row">
+				<div class="form-group col-md-12">
+					<label for="inputDescription">Description</label>
+					<textarea rows="3" class="form-control" name="inputDescription" id="inputDescription" placeholder="Description"></textarea>
+				</div>
+			</div>
+			<button type="submit" class="btn btn-default">Delete</button>
+			</div>
+			
+		    <?php echo form_close();?>
+			
+		</div>
+		<div class="col-md-3">
+			<p>
+				Sed accumsan augue a lacus luctus condimentum. Aenean aliquam id sapien id molestie. 
+				Maecenas eu pharetra turpis. Aenean nec facilisis odio. Mauris sodales sodales suscipit. 
+				Morbi rhoncus elit et egestas interdum. Ut et magna ac nisi ultrices mattis in sit amet leo. 
+				Nunc consectetur egestas ligula, id vulputate ligula congue id. Pellentesque nec hendrerit arcu, at accumsan massa. 
+				Etiam eu varius purus. Vivamus et purus risus. Nam vulputate tempus nulla, id tempor quam pellentesque sed. 
+				Etiam mi est, tempus in dapibus sit amet, ultrices et nibh.
+			</p>
+		</div>
+	</div>
